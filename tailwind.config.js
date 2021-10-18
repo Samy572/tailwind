de79267bc439b0a1b9d51f4ba14414be51119591
Samy Chabbi <samychabbi@gmx.fr>
2021-10-18 10:43:05 +0200

    tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors:{
+        blue:{
+          900:"#9bf6ff",
+        }
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
